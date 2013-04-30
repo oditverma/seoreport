44e251acce75e4c8f60d95ad8bccb6f26dc5440d
oditverma <oditverma@gmail.com>
2013-04-30 17:58:06 +0530

    chnages

--- application/forms/EmailForm.php
+++ application/forms/EmailForm.php
@@ -3,14 +3,14 @@
 class Application_Form_EmailForm extends Zend_Form {
 
     public function init() {
-        $this->setAttrib('class', 'form-signin')->setAttrib('style','margin-top:20px;');
+      
         $validator = new Zend_Validate_EmailAddress(Zend_Validate_Hostname::ALLOW_DNS |
                 Zend_Validate_Hostname::ALLOW_LOCAL);
         $validator->setOptions(array('domain' => FALSE))->getHostnameValidator()
                 ->setValidateIdn(false);
         $email = $this->createElement('text', 'email')->setRequired(TRUE)
-                ->setAttrib('placeholder', 'Email')
-                ->setLabel('E-mail : ')->addFilters(array('StringTrim', 'StripTags'))
+                ->setAttrib('placeholder', 'Email')->setAttrib('class','verimail-error')->setAttrib('required','required')
+                ->addFilters(array('StringTrim', 'StripTags'))
                 ->addValidator('EmailAddress', TRUE);
         $submit = $this->createElement('submit', 'submit')->setAttrib('class', 'btn btn-primary');
         $this->addElements(array($email, $submit));
