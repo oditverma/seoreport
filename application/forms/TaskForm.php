9d6a4d06030cb80627fc6adb0cc670adb499ad23
oditverma <oditverma@gmail.com>
2013-05-22 18:45:49 +0530

    changes

--- application/forms/TaskForm.php
+++ application/forms/TaskForm.php
@@ -7,8 +7,10 @@ class Application_Form_TaskForm extends Zend_Form {
         $description = $this->createElement('textarea', 'description')->setRequired(true)->setAttribs(array('rows' => 5, 'cols' => 5))->setAttrib('placeholder', 'Add Description')->setLabel('Add Description');
         $add = $this->createElement('text', 'added_by')->setAttrib('placeholder', 'Added By')->setLabel('Added By :- ')->setRequired(true);
         $assign = $this->createElement('text', 'assign_to')->setAttrib('placeholder', 'Assign To')->setLabel('Assign To :- ')->setRequired(true);
-        $time_added = $this->createElement('text', 'time_added')->setAttrib('placeholder', 'HH:mm:ss')->setLabel('Time Added :- ');
-        $time_completed = $this->createElement('text', 'time_completed')->setAttrib('placeholder', 'HH:mm:ss')->setLabel('Time Completed :- ');
+        $time_added = $this->createElement('text', 'time_added')->setAttrib('placeholder', 'yyyy/MM/dd HH:mm:ss PP')->setAttrib('data-format', 'yyyy/MM/dd HH:mm:ss PP')
+                        ->setLabel('Time Added :- ')->setDecorators(array('ViewHelper'));
+        $time_completed = $this->createElement('text', 'time_completed')->setAttrib('placeholder', 'yyyy/MM/dd HH:mm:ss PP')
+                ->setAttrib('data-format', 'yyyy/MM/dd HH:mm:ss PP')->setDecorators(array('ViewHelper'));
         $file = new Zend_Form_Element_File('attachment');
         $file->setDestination(APPLICATION_PATH . '/../public/uploads')
                 ->setRequired(true);
